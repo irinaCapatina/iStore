c81da54aa312d987d3853d147d5f43ac5d745f4d
Irina Capatina <irina.capatina@yahoo.com>
2017-01-26 17:12:17 +0200

    commit on iStore

--- iStore/public/components/main/controller/ProductsController.js
+++ iStore/public/components/main/controller/ProductsController.js
@@ -6,9 +6,30 @@
 	function ProductsController (ConfigService, Api, $stateParams, $state){
 		var self = this;
 		self.dataB = [];
+		self.basket = [];
+		self.total = 0;
 
-		self.getProduct = function getProduct(id) {
-			$state.go('details',  {id:id});
+		self.addToBasket = function addToBasket(product) {
+			self.basket.push(product);
+			self.total += parseFloat(product.price);
+		};
+
+		self.getProductsCount = function() {
+			var ctrlLength = self.basket.length;
+			if(ctrlLength === 0) {
+				return 'Empty';
+			} else if(ctrlLength > 1) {
+				return ctrlLength + 'items';
+			} else if(ctrlLength === 1) {
+			return ctrlLength + 'item';
+			}
+		};
+
+		self.deleteProduct =  function deleteProduct(product) {
+			self.basket.splice(self.basket.indexOf(product), 1);
+			if (self.total > 0) {
+				self.total -= parseFloat(product.price);
+			}
 		};
 
 		self.getAllProducts = function getAllProducts() {
